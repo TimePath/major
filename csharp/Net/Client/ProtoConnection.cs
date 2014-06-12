533a8e65284d19d23424c4bc24bfcab570187ac6
TimePath <andrew.hardaker1995@gmail.com>
2014-06-12 19:59:11 +1000

    Revise locks

--- csharp/Net/Client/ProtoConnection.cs
+++ csharp/Net/Client/ProtoConnection.cs
@@ -12,24 +12,26 @@ using Google.ProtocolBuffers;
 
 namespace Net.Client
 {
+    /// <summary>
+    /// Decorated methods are considered in the callback process.
+    /// </summary>
     public class CallbackAttribute : Attribute
     {
     }
 
     public class ProtoConnection
     {
-        private static readonly NLog.Logger logger = NLog.LogManager.GetCurrentClassLogger();
-
+        private static readonly NLog.Logger logger = NLog.LogManager.GetCurrentClassLogger ();
         private Socket connection;
         private IPEndPoint remote;
         private NetworkStream netStream;
 
-        protected ProtoConnection ()
+        internal ProtoConnection ()
         {
         }
 
         /// <summary>
-        /// Initializes a new instance of the <see cref="Net.Client.AsyncClient"/> class.
+        /// Initializes a new instance of the <see cref="Net.Client.ProtoConnection"/> class.
         /// </summary>
         /// <param name="ipEndpoint">Ip address: new IPEndPoint(IPAddress.Parse("127.0.0.1"), 9000)</param>
         public ProtoConnection (IPEndPoint ipEndpoint)
@@ -37,12 +39,15 @@ namespace Net.Client
             this.remote = ipEndpoint;
         }
 
-        public void Read ()
+        /// <summary>
+        /// Read data from the connected stream.
+        /// </summary>
+        internal void Read ()
         {
             Callback (this, Meta.ParseDelimitedFrom (netStream));
         }
 
-        private bool IsApplicable (MethodInfo method, object o)
+        internal bool IsApplicable (MethodInfo method, object o)
         {
             if (method.GetCustomAttributes (typeof(CallbackAttribute), false).Length == 0)
                 return false;
@@ -52,27 +57,65 @@ namespace Net.Client
             return c [0].ParameterType.IsAssignableFrom (o.GetType ());
         }
 
-        protected void Callback (object callbacks, Meta msg)
+        private readonly Dictionary<Type, FutureMessage> locks = new Dictionary<Type, FutureMessage> ();
+
+        private FutureMessage GetFuture (Type type)
+        {
+            lock (locks) {
+                if (!locks.ContainsKey (type)) {
+                    return locks [type] = new FutureMessage ();
+                }
+                return locks [type];
+            }
+        }
+
+        private class FutureMessage
+        {
+            private LinkedList<IMessageLite> list = new LinkedList<IMessageLite> ();
+            private EventWaitHandle notify = new AutoResetEvent (false);
+            private object readLock = new {};
+
+            public void Push (IMessageLite o)
+            {
+                lock (list) {
+                    list.AddLast (o);
+                    Notify (); // Wake up
+                }
+            }
+
+            public void Notify ()
+            {
+                if (list.Count == 0)
+                    return;
+                notify.Set ();
+            }
+
+            public IMessageLite Pop ()
+            {
+                while (true) {
+                    lock (readLock) { // One reading thread at a time
+                        notify.WaitOne ();
+                        lock (list) { // Stop writing to the list
+                            if (list.First == null) { // Must have data safeguard
+                                continue;
+                            }
+                            IMessageLite response = list.First.Value;
+                            list.RemoveFirst ();
+                            return response;
+                        }
+                    }
+                }
+            }
+        }
+
+        internal void Callback (object callbacks, Meta msg)
         {
             foreach (var f in msg.AllFields.Values) {
                 var o = f as IMessageLite;
                 if (o == null)
                     continue;
-                Type type = o.GetType ();
-                LinkedList<IMessageLite> list;
-                lock (buffer) {
-                    if (!buffer.ContainsKey (o.GetType ())) {
-                        list = buffer [type] = new LinkedList<IMessageLite> ();
-                    } else {
-                        list = buffer [type];
-                    }
-                }
-                list.AddLast (o);
-                lock (locks) {
-                    if (locks.ContainsKey (type)) {
-                        locks [type].Set ();
-                    }
-                }
+
+                GetFuture (o.GetType ()).Push (o);
 
                 MethodInfo m = null;
                 foreach (var methodInfo in callbacks.GetType ().GetMethods (BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.DeclaredOnly)) {
@@ -86,15 +129,19 @@ namespace Net.Client
                     continue;
                 }
                 try {
-                    m.Invoke (this, new object[]{ o });
+                    m.Invoke (callbacks, new object[]{ o });
                 } catch (Exception e) {
                     logger.Warn ("Callback failed for '{0}': {1}.", o.GetType (), e);
                 }
             }
         }
 
-        Dictionary<Type, ManualResetEvent> locks = new Dictionary<Type, ManualResetEvent> ();
-        Dictionary<Type, LinkedList<IMessageLite>> buffer = new Dictionary<Type, LinkedList<IMessageLite>> ();
+        private int counter;
+
+        public Meta.Builder CreateBuilder ()
+        {
+            return Meta.CreateBuilder ().SetTag (Interlocked.Increment (ref counter));
+        }
 
         /// <summary>
         /// Write the specified msg and wait for a response of type T.
@@ -102,33 +149,24 @@ namespace Net.Client
         /// </summary>
         /// <param name="msg">Message.</param>
         /// <typeparam name="T">The 1st type parameter.</typeparam>
-        public T Write<T> (IMessageLite msg) where T : IMessageLite
+        public T Write<T> (Meta msg) where T : IMessageLite
         {
-            Type type = typeof(T);
-            ManualResetEvent l;
-            lock (locks) {
-                if (!locks.ContainsKey (type)) { // Never expected one of these messages before, initialize first
-                    l = locks [type] = new ManualResetEvent (false);
-                } else { // Re-use existing
-                    l = locks [type];
-                    l.Reset ();
-                }
-            }
             Write (msg);
-            l.WaitOne ();
-            lock (buffer) {
-                IMessageLite response = buffer [type].First.Value;
-                buffer [type].RemoveFirst ();
-                return (T)response;
-            }
+            Type type = typeof(T);
+            return (T)GetFuture (type).Pop ();
         }
 
         public void Write (IMessageLite msg)
         {
-            msg.WriteDelimitedTo (netStream);
+            if (netStream != null) // Allow for testing without a server
+                msg.WriteDelimitedTo (netStream);
         }
 
-        public void Connect (bool block)
+        /// <summary>
+        /// Connect the client and begin reading on a new thread.
+        /// </summary>
+        /// <param name="blocking">If set to <c>true</c>, blocks until connected.</param>
+        public void Connect (bool blocking)
         {
             ManualResetEvent initialized = new ManualResetEvent (false);
             connection = new Socket (AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
@@ -140,9 +178,15 @@ namespace Net.Client
 
                 netStream = new NetworkStream (connection, false);
                 new Thread (() => {
+                    Thread.CurrentThread.Name = "Networking";
+                    int timeout = 5;
                     while (true) {
                         // Await data
-                        if (!connection.Poll (-1, SelectMode.SelectRead)) {
+                        if (!connection.Poll (timeout * 1000000, SelectMode.SelectRead)) {
+                            logger.Warn ("{0} seconds of inactivity", timeout);
+                            foreach (var l in locks.Values) {
+                                l.Notify ();
+                            }
                             continue;
                         }
                         Read ();
@@ -150,7 +194,7 @@ namespace Net.Client
                 }).Start ();
 
             }, null);
-            if (block) {
+            if (blocking) {
                 initialized.WaitOne (); // Block until connected
             }
         }
--- csharp/Net/Client/Test.cs
+++ csharp/Net/Client/Test.cs
@@ -6,17 +6,70 @@ using System.Collections.Generic;
 
 namespace Net.Client
 {
+    class CallbackMonitor
+    {
+        [Callback] void test (ListResponse d)
+        {
+            var i = 0;
+        }
+
+        [Callback] void test (FileChunk d)
+        {
+            var i = 0;
+        }
+    }
+
     class Program
     {
         private static readonly NLog.Logger logger = NLog.LogManager.GetCurrentClassLogger ();
 
         public static void Main (string[] args)
         {
+            Threading ();
+            //Benchmark ();
+        }
+
+        static void Threading ()
+        {
+            var c = new ProtoConnection ();
+            var callbacks = new CallbackMonitor ();
+            Random random = new Random();
+            new Thread (() => {
+                while (true) {
+                    c.Callback (callbacks, c.CreateBuilder ()
+                        .SetFiles (ListResponse.CreateBuilder ().Build ())
+                        .Build ());
+                    Thread.Sleep(random.Next(0, 10));
+                }
+            }).Start ();
+            new Thread (() => {
+                while (true) {
+                    c.Write<ListResponse> (null);
+                    Thread.Sleep(random.Next(0, 10));
+                }
+            }).Start ();
+            new Thread (() => {
+                while (true) {
+                    c.Callback (callbacks, c.CreateBuilder ()
+                        .SetChunk(FileChunk.CreateBuilder().Build())
+                        .Build ());
+                    Thread.Sleep(random.Next(0, 10));
+                }
+            }).Start ();
+            new Thread (() => {
+                while (true) {
+                    c.Write<FileChunk> (null);
+                    Thread.Sleep(random.Next(0, 10));
+                }
+            }).Start ();
+        }
+
+        static void Benchmark ()
+        {
             ProtoConnection c = new ProtoConnection (new IPEndPoint (IPAddress.Parse ("127.0.0.1"), 9001));
             c.Connect (true);
             for (int i = 0; i < 100000; i++) {
-                ListResponse fl = c.Write<ListResponse> (Meta.CreateBuilder ()
-                    .SetTag (i)
+                ListResponse fl = c.Write<ListResponse> (c.CreateBuilder ()
                     .SetListRequest (ListRequest.CreateBuilder ()
                         .SetPath ("/")
                         .Build ())
